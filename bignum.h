13516882e57d21fe4591b4000fccd9dd95acf39f
KueiYuChen <chen883588@gmail.com>
2017-08-01 11:58:23 +0800

    Update bignum.h

--- bignum.h
+++ bignum.h
@@ -6,7 +6,7 @@ protected:
 	int head_addr(char*);
 	BIG_NUM shift();
 	BIG_NUM shift(int);
-	bool bigger(BIG_NUM&);
+	bool bigger(BIG_NUM);
 public:
 	BIG_NUM();
 	BIG_NUM(char*);
@@ -64,6 +64,43 @@ BIG_NUM BIG_NUM::operator-(BIG_NUM a){
 		return a;
 	}
 }
+BIG_NUM BIG_NUM::operator*(BIG_NUM a){
+	int addr=head_addr(num)<head_addr(a.num)?head_addr(num):head_addr(a.num);
+	BIG_NUM temp;
+	for(int i=length-1;i>=addr;i--){
+		for(int j=0;j<(a.num[i]-'0');j++){
+			temp=temp+this->shift(length-i-1);
+		}
+	}
+	return temp;
+}
+BIG_NUM BIG_NUM::operator/(BIG_NUM a){
+	int t_len=length-head_addr(num);
+	int a_len=length-head_addr(a.num);
+	BIG_NUM ans,temp(*this);
+	for(int i=(t_len-a_len);i>=0;i--){
+		while(temp.bigger(a.shift(i))){
+			printf("temp=   ");
+			temp.print();
+			printf("a.shift=");
+			a.shift(i).print();
+			printf("ans=");
+			ans.print();
+			temp=temp-a.shift(i);
+			ans.num[length-i-1]++;
+		}
+	}
+	return ans;
+}
+BIG_NUM BIG_NUM::operator%(BIG_NUM a){
+	BIG_NUM temp;
+//	temp=(a*((*this)/a));
+//	temp=*this-temp;
+	temp=(*this)/a;
+	temp=a*temp;
+	return *this;
+}
+
 void BIG_NUM::print(){
 	for(int i=head_addr(num);i<length;i++)
 		printf("%c",num[i]);
@@ -95,13 +132,17 @@ BIG_NUM BIG_NUM::shift(int x){
 		temp.num[i-x]=num[i];
 	return temp;
 }
-bool BIG_NUM::bigger(BIG_NUM& a){
+bool BIG_NUM::bigger(BIG_NUM a){
 	int t_addr=head_addr(num);
 	int a_addr=head_addr(a.num);
 	if(t_addr>a_addr)
 		return false;
-	if(num[t_addr]<a.num[t_addr])
-		return false;
-	else
+	else if(t_addr<a_addr)
 		return true;
+		
+	for(t_addr;t_addr<length;t_addr++){
+		if(num[t_addr]<a.num[t_addr])
+			return false;
+	}
+			return true;	
 }
